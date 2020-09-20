1370fb6bdbcb7870a2824c379c72c7c6d6c23c2f
최홍희 <vvshinevv@gmail.com>
2020-09-20 20:34:22 +0900

    파이썬 기본 문법

---
+++ overview/practice.py
@@ -0,0 +1,35 @@
+def print_param(name):
+    print(f'Hi, {name}')
+
+
+def dictionary():
+    data = dict()
+    data['사과'] = 'Apple'
+    data['바나나'] = 'Banana'
+    data['코코넛'] = 'Coconut'
+    if '사과' in data:
+        print('데이터 존재')
+
+    print(list(data.keys()))
+    print(data.values())
+
+
+def input_test():
+    a, b, c, d, e = map(int, input().split())
+    print(a, b, c, d, e)
+
+
+def input_2_test():
+    n = int(input())
+    m = int(input())
+
+    arr = []
+    for i in range(n):
+        arr.append(list(map(int, input().split())))
+
+
+if __name__ == '__main__':
+    print_param('PyCharm')
+    dictionary()
+    # input_test()
+    input_2_test()
